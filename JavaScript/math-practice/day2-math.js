140fb892955ee533da62bca1bde30b1a86cf1a89
Kumar-Prasannajit-Sahu <kumarprasannajitsahu@gmail.com>
2025-04-07 00:46:53 +0530

    🎲 Day 2 - Math practice with random number & even/odd check

---
+++ JavaScript/math-practice/day2-math.js
@@ -0,0 +1,8 @@
+// Random Number between 1 and 100
+let num1 = Math.floor(Math.random() * 100) + 1;
+console.log("Random number between 1 and 100:", num1);
+
+// Even or Odd using ternary operator
+let num2 = 22;
+let res = num2 % 2 === 0 ? "even" : "odd";
+console.log(`${num2} is ${res}`);

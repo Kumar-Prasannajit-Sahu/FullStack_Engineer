5cb075bc8067f8023be2fcdbd056f351176d81c2
Kumar-Prasannajit-Sahu <kumarprasannajitsahu@gmail.com>
2025-04-07 00:36:29 +0530

    🧹 Reinitialized repo - folder structure & day-wise practice

---
+++ JavaScript/string-practice/string.js
@@ -0,0 +1,35 @@
+let naam = prompt("Enter your name: ");
+console.log("Hello, " + naam + "! Welcome to the JavaScript world.");   
+console.log("Your name has " + naam.length + " characters.");
+console.log("Your name in uppercase: " + naam.toUpperCase()); 
+let msg = ' Hello JS World ';
+console.log(msg.trim()); // Remove whitespace from both sides
+console.log(`your string has ${msg.trim().length} characters`); // Template literals
+let msg2 = 'I love JavaScript';
+msg2 = msg2.replace('love', 'enjoy'); // Replace 'love' with 'enjoy'
+console.log(`${msg2} me 'python' string he kya ? : ${msg2.includes('python')}`); // Check if 'python' is in msg2
+let msg3 = "This is great";
+let msg4 = msg3.repeat(3); // Repeat msg3 three times
+console.log(msg4.slice(5,15)); // Extract substring from index 5 to 15
+// let age = prompt("Enter your age: ");
+// let city = prompt("Enter your city: ");
+let user = prompt("Enter your name again: ");
+for (let i = 0; i<user.length; i++){
+    console.log(user[i]); // Print each character of the name on a new line
+    console.log(user[i].toUpperCase()); // Print each character in uppercase
+}
+
+let newSentence = "";
+let sentence = "I am learning JavaScript";
+for (let i = 0; i<sentence.length;i++){
+    if (i === 0 || sentence[i-1] === ' '){
+        newSentence += sentence[i].toUpperCase(); // Capitalize the first letter of each word
+    }
+    else{
+        newSentence += sentence[i]; // Keep the rest of the letters as they are
+    }
+}
+
+let msg5 = ' Welcome User ';
+console.log(msg5.trimStart());
+console.log(msg5.trimEnd()); // Remove whitespace from the start and end of the string

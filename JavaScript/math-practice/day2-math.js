9173a6af61df66f8cb08b0cd514393677611b343
Kumar-Prasannajit-Sahu <kumarprasannajitsahu@gmail.com>
2025-04-07 00:51:00 +0530

    ➕ Day 2 - Arithmetic operations

--- JavaScript/math-practice/day2-math.js
+++ JavaScript/math-practice/day2-math.js
@@ -6,3 +6,13 @@ console.log("Random number between 1 and 100:", num1);
 let num2 = 22;
 let res = num2 % 2 === 0 ? "even" : "odd";
 console.log(`${num2} is ${res}`);
+
+// Question 3: Perform basic arithmetic operations between two numbers
+let a = 10;
+let b = 3;
+
+console.log("Addition:", a + b);         // 13
+console.log("Subtraction:", a - b);      // 7
+console.log("Multiplication:", a * b);   // 30
+console.log("Division:", a / b);         // 3.33
+console.log("Modulus:", a % b);          // 1

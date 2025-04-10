d099a0aa414a07d564889a8a7beaedcc7c4a1353
Kumar-Prasannajit-Sahu <kumarprasannajitsahu@gmail.com>
2025-04-11 01:15:52 +0530

    🟩 Practiced sum of elements -reduce

--- day6-practice/day6.js
+++ day6-practice/day6.js
@@ -8,3 +8,6 @@ let oddArr = arr.filter(num => num % 2 !== 0);
 let squaredOddArr = oddArr.map(num => num*num);
 let sumOfSquaresOdd = squaredOddArr.reduce((acc,curr) => acc + curr, 0);
 console.log(sumOfSquaresOdd); // Output: 35 (1^2 + 3^2 + 5^2 = 1 + 9 + 25 = 35)
+
+let sumArr = arr.reduce((acc, curr) => acc + curr, 0);
+console.log(sumArr); // Output: 21 (1 + 2 + 3 + 4 + 5 + 6 = 21)

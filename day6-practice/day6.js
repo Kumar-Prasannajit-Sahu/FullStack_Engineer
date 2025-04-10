8f08e9093c9ac07de05ff6d2e9040dc3465daacb
Kumar-Prasannajit-Sahu <22ece077.kumarprasannajitsahu@giet.edu>
2025-04-11 02:04:33 +0530

    troubleshoot

--- day6-practice/day6.js
+++ day6-practice/day6.js
@@ -11,3 +11,5 @@ console.log(sumOfSquaresOdd); // Output: 35 (1^2 + 3^2 + 5^2 = 1 + 9 + 25 = 35)
 
 let sumArr = arr.reduce((acc, curr) => acc + curr, 0);
 console.log(sumArr); // Output: 21 (1 + 2 + 3 + 4 + 5 + 6 = 21)
+console.log(arr); // Output: [1, 2, 3, 4, 5, 6]
+console.log(evenArr); // Output: [2, 4, 6]
